b787897ebbdd19710a55c42cc05707da6a4763ac
Dobrogostov <39377437+Dobrogostov@users.noreply.github.com>
2018-09-16 13:28:11 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Salut ${name} ! Bienvenue sur France Airdrop ! Je vous invite a rejoindre l'Airdrop avec le plus de potentiel d'après moi : X-cash ! 20 mois d'airdrops, inscrivez vous pour le mois de septembre via mon lien pour obtenir 25% bonus : bit.ly/2xdAHBa ! NE LE LOUPEZ PAS *** The Best Airdrop of the year for me, the only one you need to do : X-cash ! Register for september with my link to get 25% bonus : bit.ly/2xdAHBa DON'T MISS IT`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
